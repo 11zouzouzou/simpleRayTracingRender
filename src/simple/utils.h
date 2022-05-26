2e9337b7b9d96a50e3082e0867bd3a64a85bc519
zouzouzou11 <1096174991@qq.com>
2022-05-26 09:49:47 +0800

    add random function

--- src/simple/utils.h
+++ src/simple/utils.h
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <cstdlib>
 
 // usings
 
@@ -20,6 +21,25 @@ const double pi = 3.1415926535897932385;
 inline double degrees_to_radians(double degrees)
 {
     return degrees * pi / 180.0;
+}
+
+inline double random_double()
+{
+    //[0,1)
+    return rand() / (RAND_MAX + 1.0);
+}
+
+// #include <random>
+
+// inline double random_double() {
+//     static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+//     static std::mt19937 generator;
+//     return distribution(generator);
+// }
+
+inline double random_double(double min, double max)
+{
+    return min + (max - min) * random_double();
 }
 
 // Common Headers
