ab2031e3db828f0e6345403b1723012cf27012fe
zouzouzou11 <1096174991@qq.com>
2022-05-26 17:41:17 +0800

    metal use fuzz

--- src/SimpleMain.cpp
+++ src/SimpleMain.cpp
@@ -145,12 +145,14 @@ int main()
     // material
     auto material_center = make_shared<lambertian_material>(color(1.0, 1.0, 0.0));
     auto material_ground = make_shared<lambertian_material>(color(1, 1, 1));
-    auto material_left = make_shared<metal_material>(color(1, 1, 1));
-        //构造场景
-        // World
-        hittable_list world;
+    auto material_left = make_shared<metal_material>(color(1, 1, 1), 0.0);
+    auto material_right = make_shared<metal_material>(color(1, 0, 1), 0.8);
+    //构造场景
+    // World
+    hittable_list world;
     world.add(make_shared<sphere>(point3(0, 0, -1), 0.5, material_center));
     world.add(make_shared<sphere>(point3(-1, 0, -1), 0.5, material_left));
+     world.add(make_shared<sphere>(point3(1, 0, -1), 0.5, material_right));
     world.add(make_shared<sphere>(point3(0, -100.5, -1), 100.0, material_ground));
 
     //抗锯齿周围像素样本采样数
