52f75794d39526d968c4711109ae72ea5ffef22f
zouzouzou11 <1096174991@qq.com>
2022-05-30 11:38:54 +0800

    perlin use linear

--- src/simple/perlin.h
+++ src/simple/perlin.h
@@ -2,7 +2,7 @@
 #define PERLIN_H
 #include "utils.h"
 /**
- * @brief 
+ * @brief
  * https://zhuanlan.zhihu.com/p/206271895
  */
 class perlin
@@ -31,10 +31,29 @@ public:
 
     double noise(const point3 &p) const
     {
-        auto i = static_cast<int>(4 * p.x()) & 255;//得到梯度值
-        auto j = static_cast<int>(4 * p.y()) & 255;
-        auto k = static_cast<int>(4 * p.z()) & 255;
-        return ranfloat[perm_x[i] ^ perm_y[j] ^ perm_z[k]];
+        //直接获取0~255梯度值赋值
+        // auto i = static_cast<int>(4 * p.x()) & 255;
+        // auto j = static_cast<int>(4 * p.y()) & 255;
+        // auto k = static_cast<int>(4 * p.z()) & 255;
+        // return ranfloat[perm_x[i] ^ perm_y[j] ^ perm_z[k]];
+        //平滑差值
+        auto u = p.x() - floor(p.x()); //得到小数
+        auto v = p.y() - floor(p.y());
+        auto w = p.z() - floor(p.z());
+
+        auto i = static_cast<int>(floor(p.x())); //得到最大整数
+        auto j = static_cast<int>(floor(p.y()));
+        auto k = static_cast<int>(floor(p.z()));
+        double c[2][2][2];
+        //单位正方体循环
+        for (int di = 0; di < 2; di++)
+            for (int dj = 0; dj < 2; dj++)
+                for (int dk = 0; dk < 2; dk++)
+                    //搜集临近三维晶体结果的8个顶点的梯度值
+                    c[di][dj][dk] = ranfloat[perm_x[(i + di) & 255] ^
+                                             perm_y[(j + dj) & 255] ^
+                                             perm_z[(k + dk) & 255]];
+        return trilinear_interp(c, u, v, w);
     }
 
 private:
@@ -66,6 +85,20 @@ private:
             p[i] = p[target];
             p[target] = tmp;
         }
+    }
+
+    static double trilinear_interp(double c[2][2][2], double u, double v, double w)
+    {
+        //三线性插值
+        auto accum = 0.0;
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 2; j++)
+                for (int k = 0; k < 2; k++)
+                    accum += (i * u + (1 - i) * (1 - u)) *
+                             (j * v + (1 - j) * (1 - v)) *
+                             (k * w + (1 - k) * (1 - w)) * c[i][j][k];
+
+        return accum;
     }
 };
 #endif
